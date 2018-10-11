a9d259ca6e89abde828723efed97abb32fa83938
MeganGuidry <mguid73@lsu.edu>
2018-10-11 11:47:32 -0700

    adding week8 in class assignment

---
+++ revComp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# input DNA sequence in all caps
+complement = []
+dnaSeq = input ("Input DNA sequence in all capital letters: ")
+
+#reverses DNA sequence that user inputs
+dnaSeq = dnaSeq[::-1]
+print (dnaSeq)
+
+#create compliment of dna Sequence
+for i in dnaSeq:
+	if i == "A":
+		complement.append("T")
+	elif i == "C":
+		complement.append("G")
+	elif i == "T":
+                complement.append("A")
+	elif i == "G":
+                complement.append("C")
+
+#converts list to a string
+str2 = ''.join(complement)
+print (str2)
