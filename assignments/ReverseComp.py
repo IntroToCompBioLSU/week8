92662e5ac9191bac6b048d0c2b2949c0f50b3aad
jembrown <jeremymbrown@gmail.com>
2018-12-09 13:06:39 -0600

    Adding comments for in-class assignment

--- assignments/ReverseComp.py
+++ assignments/ReverseComp.py
@@ -1,4 +1,9 @@
-#! /usr/bin/python
+#! /usr/bin/env python
+
+# DB: I couldn't figure out why I was having trouble with the inputs for your scripts, but
+#     it was because you used /usr/bin/python in your shebang line. For me, that calls 
+#     version 2, while /usr/bin/env python calls version 3. Maybe stick with the latter
+#     because it should be more robust across different systems.
 
 mystring = input("Enter Your DNA Sequence: ")
 #converts to lowercase
@@ -11,3 +16,5 @@ mystring = mystring.replace("t", "A")
 #print reverse order
 print("Your reverse complimentary DNA Sequence is: ")
 print(mystring[::-1])
+
+# DB: Overall, good! In general, for aesthetics, I'd suggest adding spaces between lines.
