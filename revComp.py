83939c35c66447963dde1a721ce7fc18890e1c1e
jembrown <jeremymbrown@gmail.com>
2018-12-09 21:15:14 -0600

    Comments added for rev comp script

--- revComp.py
+++ revComp.py
@@ -3,6 +3,7 @@
 # input DNA sequence in all caps
 complement = []
 dnaSeq = input ("Input DNA sequence in all capital letters: ")
+dnaSeq = dnaSeq.upper()
 
 #reverses DNA sequence that user inputs
 dnaSeq = dnaSeq[::-1]
@@ -14,12 +15,15 @@ for i in dnaSeq:
 	elif i == "C":
 		complement.append("G")
 	elif i == "T":
-                complement.append("A")
+		complement.append("A")		# DB: Best to keep indentation style the same
 	elif i == "G":
-                complement.append("C")
+		complement.append("C")
 
 
 #converts list to a string
-str = ''.join(complement)
-print("reverse complement: ") 
+str = ''.join(complement)		# DB: It's good practice to avoid generic variable names
+print("reverse complement: ")	#     like 'str'. Maybe use 'revComp'?
 print(str)
+
+# DB: Good! You could make it a little more general by using .upper() to automatically
+#     convert the sequence to uppercase (see above)
